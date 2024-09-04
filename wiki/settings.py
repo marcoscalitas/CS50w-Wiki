c8adde9f9a69d24dbb501eaf86351c6d46a2afa6
Marcos Calitas <marcosfredericocalitas22@gmail.com>
2024-09-04 02:21:25 +0100

    chore: edit static root path to staticfile

--- wiki/settings.py
+++ wiki/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
