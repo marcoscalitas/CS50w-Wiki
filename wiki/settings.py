b307786797100d5a5203daf39b644a034c9a98b3
Marcos Calitas <marcosfredericocalitas22@gmail.com>
2024-09-28 18:27:08 +0100

    refactor: change project deployment settings

--- wiki/settings.py
+++ wiki/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "cs50w-wiki.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
